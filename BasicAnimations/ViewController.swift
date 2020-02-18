2ee56821c984b32be3dab99739bd132b2442982a
scottharris86 <scottharrisdev@gmail.com>
2020-02-18 12:00:48 -0500

    Initial Commit

---
+++ BasicAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicAnimations
+//
+//  Created by scott harris on 2/18/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
