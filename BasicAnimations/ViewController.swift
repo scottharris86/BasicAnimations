c11d05716e519d32437ec7dc14b57ff31a0dcb3b
scottharris86 <scottharrisdev@gmail.com>
2020-02-18 12:14:36 -0500

    added emoji to center of screen

--- BasicAnimations/ViewController.swift
+++ BasicAnimations/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    var label: UILabel!
+    
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureLabel()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        label.center = view.center
+    }
+    
+    
+    // MARK: - Private
+    
+    private func configureLabel() {
+        label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        label.widthAnchor.constraint(equalTo: label.heightAnchor).isActive = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        label.text = "💃🏻"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 96)
+        label.layer.borderWidth = 2
+        label.layer.cornerRadius = 12
+        
+    }
 }
 
